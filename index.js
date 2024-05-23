7537baad661f1c7975a954264994a5d55831503d
Matheus S. Heck <matheus@qualitys.com.br>
2024-05-23 02:13:32 -0300

    uncommenting index.js

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ let synchronizer = new Synchronizer();
 let mongo = new MongoDbConnection();
 
 async function main(){    
-    // await synchronizer.synchronizeMySqlAndMongoDb();
+    await synchronizer.synchronizeMySqlAndMongoDb();
 
-    // await mongo.findAllEmployeesFromCurrentManager(110567, '');
-    // await mongo.findAllEmployeesFromCurrentManager('', 'Leon');
-    // await mongo.findAllEmployeesFromCurrentManager(110567, 'Oscar');
-    // await mongo.findAllEmployeesLinkedToTitleAllTime('Senior Engineer');
+    await mongo.findAllEmployeesFromCurrentManager(110567, '');
+    await mongo.findAllEmployeesFromCurrentManager('', 'Leon');
+    await mongo.findAllEmployeesFromCurrentManager(110567, 'Oscar');
+    await mongo.findAllEmployeesLinkedToTitleAllTime('Senior Engineer');
     await mongo.findAllEmployeesLinkedToCurrentDept('d001');
-    // await mongo.getAverageSalaryByDept('d001');
-    // await mongo.getAverageSalaryByDept('d002');
-    // await mongo.getAverageSalaryByDept('d003');
-    // await mongo.getAverageSalaryByDept('d004');
-    // await mongo.getAverageSalaryByDept('d005');
-    // await mongo.getAverageSalaryByDept('d006');
-    // await mongo.getAverageSalaryByDept('d007');
-    // await mongo.getAverageSalaryByDept('d008');
-    // await mongo.getAverageSalaryByDept('d009');
+    await mongo.getAverageSalaryByDept('d001');
+    await mongo.getAverageSalaryByDept('d002');
+    await mongo.getAverageSalaryByDept('d003');
+    await mongo.getAverageSalaryByDept('d004');
+    await mongo.getAverageSalaryByDept('d005');
+    await mongo.getAverageSalaryByDept('d006');
+    await mongo.getAverageSalaryByDept('d007');
+    await mongo.getAverageSalaryByDept('d008');
+    await mongo.getAverageSalaryByDept('d009');
 }
 
 main();
